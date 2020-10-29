e092aacaac3c35e9a8363271b56107c7484fa9bf
andybeet <22455149+andybeet@users.noreply.github.com>
2020-10-29 18:34:23 -0400

    vectorized capitlaize_first_letter function

--- R/capitalize_first_letter.R
+++ R/capitalize_first_letter.R
@@ -1,26 +1,38 @@
 #' Capitalize first letter of string
-#' 
-#' Convert a string to lower case except first letter of the first word
-#' 
-#' @param phrase Character string. Any string of characters
-#' 
-#' @return string
-#' 
+#'
+#' Convert a string to lower case except first letter of the first word.
+#'
+#' @param phrase Vector of Character string. Any string of characters
+#'
+#' @return Vector of strings
+#'
 #' @export
 
 capitalize_first_letter <- function(phrase) {
-  s <- strsplit(tolower(phrase), "\\s+")[[1]]
-  if (nchar(substring(s[1],2)) > 0 ) { # multiple letters
-    if (length(s) == 1) { #single word
-      str <- paste0(toupper(substring(s[1],1,1)),substring(s[1],2))
+
+  retstr <- NULL
+  for (irow in 1:length(phrase)) {
+
+    if (is.na(phrase[irow])) {
+      str <- "NA"
     } else {
-      #  str <- paste(paste0(toupper(substring(s[1],1,1)),substring(s[1],2)),s[-1],sep=" ",collapse=" ")
-      str <- capture.output(cat(paste0(toupper(substring(s[1],1,1)),substring(s[1],2)),s[-1]))
+
+      s <- strsplit(tolower(phrase[irow]), "\\s+")[[1]]
+      if (nchar(substring(s[1],2)) > 0 ) { # multiple letters
+        if (length(s) == 1) { #single word
+          str <- paste0(toupper(substring(s[1],1,1)),substring(s[1],2))
+        } else {
+          #  str <- paste(paste0(toupper(substring(s[1],1,1)),substring(s[1],2)),s[-1],sep=" ",collapse=" ")
+          str <- capture.output(cat(paste0(toupper(substring(s[1],1,1)),substring(s[1],2)),s[-1]))
+        }
+      } else { # single letter
+
+        str <- paste0(toupper(substring(s[1],1,1)),s[-1])
+      }
     }
-  } else { # single letter
-    
-    str <- paste0(toupper(substring(s[1],1,1)),s[-1])
+
+    retstr <- c(retstr,str)
   }
-  
-  return(str)
+
+  return(retstr)
 }
